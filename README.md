93657fa430b107e5b4ef22465db02008af297c56
zvecr <z.zvecr@gmail.com>
2017-03-17 23:57:14 +0000

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Arduino-JenkinsNotify
-Arduino based Jenkins Notifications
+RGB Arduino based Jenkins Notifications
+![Example gidf](images/example.gif)
+
+# Requirements
+## Hardware
+[Arduino mega 2560](https://www.amazon.co.uk/dp/B0046AMGW0) or compatible clone
+[Ethernet W5100 Network Shield](https://www.amazon.co.uk/dp/B00CQN2FDY/) or compatible clone
+IR LED
+
+Supported notification hardware:
+* RGB lights driven via a 24 key IR remote
+![24 button remote](images/arduino_fritzing.png)
+* [Pac-Man Plastic Ghost Lamp](https://www.amazon.co.uk/dp/B00IFC2YCC/)
+
+## Jenkins
+Install jenkins plugin https://plugins.jenkins.io/notification
+
+## Arduino
+Install library dependencies
+* ArduinoJson
+* IRremote
+* [WebServer](https://github.com/sirleech/Webduino) - requires manual install
+
+# Setup
+## Hardware
+![Arduino fritzing](images/arduino_fritzing.png)
+
+## Jenkins
+Configure job with the following settings
+* Format:JSON
+* PROTOCOL:HTTP
+* EVENT:any
+* URL:http://${IP}/status
+
+![Jenkins Config](images/jenkins_config.png)
+
+## Arduino
+1. Change IP and mac in jenkins_notify.ino
+2. Upload sketch to Arduino
---
+++ jenkins_notify.ino
@@ -0,0 +1,155 @@
+#include <Ethernet.h>
+#include <ArduinoJson.h>
+#include <IRremote.h>
+#include <WebServer.h>
+//#include "WebServer.h"
+
+// Enter a MAC address for your controller below.
+byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
+static uint8_t ip[] = { 192, 168, 1, 25 };
+WebServer webserver("", 80);
+
+IRsend irsend;
+// color codes from - https://github.com/alistairallan/RgbIrLed/blob/master/readme
+unsigned long WHITE = 0xF7E01F;
+unsigned long GREEN = 0xF7A05F;
+unsigned long RED = 0xF720DF;
+unsigned long YELLOW = 0xF730CF;
+
+enum State {PENDING, SUCCESS, UNSTABLE, FAILURE};
+
+#ifdef DEBUG
+/** 
+ * Utility function to print address
+ */
+inline static void printIPAddress()
+{
+  Serial.println(F("*****"));
+  Serial.println(Ethernet.localIP());
+  Serial.println(F("*****"));
+}
+#else
+#define printIPAddress()
+#endif
+
+/**
+ * Callback for a state change
+ */
+void updateStatus(State state)
+{
+  switch(state)
+  {
+    case SUCCESS:
+      irsend.sendNEC(GREEN, 32);
+      break;
+    case UNSTABLE:
+      irsend.sendNEC(YELLOW, 32);
+      break;
+    case FAILURE:
+      irsend.sendNEC(RED, 32);
+      break;
+    default:
+      irsend.sendNEC(WHITE, 32);
+      break;
+  }
+}
+
+/**
+ * Process a http request
+ */
+void statusCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete)
+{
+  if (type != WebServer::POST)
+  {
+    server.httpFail();
+    server.printf(F("invalid connection type"));
+    return;
+  }
+
+  // read all data
+  char json[4096] = {0};
+  int count = 0;
+  char ch = 0;
+  while ((ch = server.read()) != -1) {
+    json[count++] = ch;
+  }
+
+  // parse to json
+  StaticJsonBuffer<1024> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(json);
+  if (!root.success())
+  {
+    server.httpFail();
+    server.printf(F("parseObject() failed"));
+    return;
+  }
+
+  statusJsonCmd(server, root);
+}
+
+/**
+ * Process a json post request
+ */
+void statusJsonCmd(WebServer &server, const JsonObject& root)
+{
+  // extract args from request
+  const char* build_status = root["build"]["status"];
+  const char* build_phase = root["build"]["phase"];
+
+  // convert to state
+  State cur_status = PENDING;
+  if (strcmp(build_phase, "COMPLETED") == 0)
+  {
+    if (strcmp(build_status, "SUCCESS") == 0)
+    {
+      cur_status = SUCCESS;
+    }
+    else if (strcmp(build_status, "UNSTABLE") == 0)
+    {
+      cur_status = UNSTABLE;
+    }
+    else if (strcmp(build_status, "FAILURE") == 0)
+    {
+      cur_status = FAILURE;
+    }
+  }
+
+  updateStatus(cur_status);
+  server.httpSuccess();
+  server.printf("status:%d", cur_status);
+}
+
+/**
+ * Arduino init entry point
+ */
+void setup()
+{
+#ifdef DEBUG
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  // this check is only needed on the Leonardo:
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+#endif
+
+  // configure
+  Ethernet.begin(mac, ip);
+  webserver.addCommand("status", &statusCmd);
+  webserver.begin();
+
+  // set initial state
+  updateStatus(PENDING);
+
+  printIPAddress();
+}
+
+/**
+ * Arduino event loop entry point
+ */
+void loop()
+{
+  // process incoming connections one at a time forever
+  webserver.processConnection();
+}
+
